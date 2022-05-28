2edcac5db5d6e59bc2d603a16960413cb30707f6
tinnamchoi <tinnam.choi@gmail.com>
2022-05-28 16:51:28 +0930

    flameshot and pavucontrol

--- config.h
+++ config.h
@@ -58,12 +58,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *sscmd[]  = { "flameshot", "gui", NULL };
 static const char *fmcmd[]  = { "pcmanfm", NULL };
 static const char *discordcmd[]  = { "discord", NULL };
 static const char *firefoxcmd[]  = { "firefox", NULL };
 static const char *spotifycmd[]  = { "spotify-adblock.sh", NULL };
 static const char *obscmd[]  = { "obs", "--startreplaybuffer", NULL };
 static const char *vscodecmd[]  = { "code", NULL };
+static const char *audiocmd[]  = { "pavucontrol", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -73,6 +75,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_s,      spawn,          {.v = spotifycmd } },
 	{ MODKEY,                       XK_r,      spawn,          {.v = obscmd } },
 	{ MODKEY,                       XK_c,      spawn,          {.v = vscodecmd } },
+	{ MODKEY,                       XK_a,      spawn,          {.v = audiocmd } },
+	{ 0,                            XK_Print,  spawn,          {.v = sscmd } },
 	{ MODKEY,                       XK_x,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ControlMask,           XK_x,      spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
